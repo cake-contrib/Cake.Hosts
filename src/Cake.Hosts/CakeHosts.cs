9015615c762f3f5804d7796480c39a575ce2e6ca
Max Vasilyev <trailmax1@gmail.com>
2017-05-09 22:39:40 +0100

    Some more ground work

---
+++ src/Cake.Hosts/CakeHosts.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using Cake.Core;
+using Cake.Core.Diagnostics;
+
+namespace Cake.Hosts
+{
+    internal class CakeHosts
+    {
+        private readonly IHostsPathProvider hostsPathProvider;
+        private readonly ICakeContext cakeContext;
+        private readonly ICakeLog log;
+
+        public CakeHosts(IHostsPathProvider hostsPathProvider, ICakeContext cakeContext, ICakeLog log)
+        {
+            this.hostsPathProvider = hostsPathProvider;
+            this.cakeContext = cakeContext;
+            this.log = log;
+        }
+
+        public void AddOrReplaceHostsRecord(String ipAddress, String domainName)
+        {
+            var fullPath = hostsPathProvider.GetHostsFilePath();
+            throw new NotImplementedException();
+        }
+
+
+        public void RemoveHostsRecord(String domainName)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void HostsRecordExists(String domainName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Cake.Hosts/IHostsPathProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace Cake.Hosts
+{
+    internal interface IHostsPathProvider
+    {
+        /// <summary>
+        /// Returns full path to the hosts file
+        /// </summary>
+        /// <returns></returns>
+        string GetHostsFilePath();
+    }
+}
