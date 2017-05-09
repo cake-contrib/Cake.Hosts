50c607075337ba500bde01d7ff2184273c7334ad
Max Vasilyev <trailmax1@gmail.com>
2017-05-09 22:28:56 +0100

    Laying the foundation - api surface

---
+++ src/Cake.Hosts/CakeHostsAliases.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Cake.Core;
+using Cake.Core.Annotations;
+
+namespace Cake.Hosts
+{
+    [CakeAliasCategory("Hosts")]
+    public static class CakeHostsAliases
+    {
+        public static void AddOrReplaceHostsRecord(this ICakeContext context, String ipAddress, String domainName)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+        public static void RemoveHostsRecord(this ICakeContext context, String domainName)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public static void HostsRecordExists(this ICakeContext context, String domainName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
