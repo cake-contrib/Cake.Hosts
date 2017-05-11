7ecb587423078ada2da3591caf982cd21fd1f305
Max Vasilyev <trailmax1@gmail.com>
2017-05-11 01:35:54 +0100

    Adding comments

--- src/Cake.Hosts/CakeHostsAliases.cs
+++ src/Cake.Hosts/CakeHostsAliases.cs
@@ -4,15 +4,40 @@ using Cake.Core.Annotations;
 
 namespace Cake.Hosts
 {
+    /// <summary>
+    /// <para>
+    /// Functionalty to operate hosts-file. Requres admin permissions and will probably fail on your general CI server.
+    /// </para>
+    /// <para>
+    /// Mostly aimed on automation scripts rather than actual project builds.
+    /// </para>
+    /// Install via:
+    /// <code>
+    ///     #addin "nuget:?package=Cake.Hosts"
+    /// </code>
+    /// </summary>
     [CakeAliasCategory("Hosts")]
     public static class CakeHostsAliases
     {
+        /// <summary>
+        /// Checks if a record already exists in hosts file
+        /// </summary>
+        /// <param name="context">The cake context.</param>
+        /// <param name="ipAddress">IP address to check</param>
+        /// <param name="domainName">Domain name to check</param>
+        /// <returns>If given parameters already constitue a record in hosts file</returns>
         public static bool HostsRecordExists(this ICakeContext context, String ipAddress, String domainName)
         {
             var cakeHosts = GetCakeHosts(context);
             return cakeHosts.HostsRecordExists(ipAddress, domainName);
         }
 
+        /// <summary>
+        /// Appends a record to the end of hosts file. If this value already exist, no modifications applied
+        /// </summary>
+        /// <param name="context">The Cake context.</param>
+        /// <param name="ipAddress">IP address to add</param>
+        /// <param name="domainName">Domain name to add</param>
         public static void AddHostsRecord(this ICakeContext context, String ipAddress, String domainName)
         {
             var cakeHosts = GetCakeHosts(context);
@@ -20,6 +45,12 @@ namespace Cake.Hosts
         }
 
 
+        /// <summary>
+        /// Removes a line with given IP address and domain name. If given pair does not exist in hosts file, no modifications applied
+        /// </summary>
+        /// <param name="context">The cake context.</param>
+        /// <param name="ipAddress">IP address to check</param>
+        /// <param name="domainName">Domain name to check</param>
         public static void RemoveHostsRecord(this ICakeContext context, String ipAddress, String domainName)
         {
             var cakeHosts = GetCakeHosts(context);
